874869a7d57e8fb1ecac393e6421bbe82e38e51b
Aikrof <aikrofstark@gmail.com>
2020-03-07 00:18:48 +0200

    changed htaccess

--- config/db.php
+++ config/db.php
@@ -4,11 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=bintime_db',
     'username' => 'root',
-    'password' => '12345',
+    'password' => 'root',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
